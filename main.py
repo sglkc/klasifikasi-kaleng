f2ee0a241d66afadeb0af4d8924b9770c694888d
Seya <31957516+sglkc@users.noreply.github.com>
2024-05-16 19:08:19 +0700

    tambah komentar

--- main.py
+++ main.py
@@ -1,141 +1,231 @@
-from typing import Sequence, Tuple
 import cv2, time
 import numpy as np
+from typing import Iterable, Sequence, Tuple
 from cv2.typing import MatLike
 
 __verbose = False
 
-def log(text, force=False):
+# Prosedur untuk menampilkan teks pada terminal
+def log(text, force=False) -> None:
     global __verbose
 
+    ## Jika verbose atau force = True, tampilkan pesan
     if __verbose or force:
         print(text, flush=True)
     else:
         print('.', end='', flush=True)
 
-def showd(citra, title='window'):
-    cv2.imshow(title, citra)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-    exit()
+# Fungsi grayscaling dengan rumus fungsi yang dapat diubah
+def grayscale(citra: MatLike,
+              rumus=lambda r,g,b:(0.2126*r)+(0.7152*g)+(0.0722*b)
+              ) -> MatLike:
 
-def grayscale(citra: MatLike, rumus=lambda r,g,b:(0.2126*r)+(0.7152*g)+(0.0722*b)):
+    ## Dapatkan tinggi dan lebar citra lalu buat citra hasil kosong
     h, w = citra.shape[:2]
     hasil = np.zeros((h, w), np.uint8)
 
+    ## Perulangan untuk setiap piksel pada citra
     for y in range(h):
         for x in range(w):
+
+            ### Dapatkan nilai biru, hijau, dan merah lalu hitung
+            ### dengan rumus
             b, g, r = citra[y, x]
             pixel = np.ceil(rumus(r, g, b))
 
-            ## Clipping
+            ### Clipping agar nilai sesuai rentang kedalaman 8 bit
             if pixel < 0: pixel = 0
             if pixel > 255: pixel = 255
 
+            ### Piksel pada koordinat ini pada citra hasil
+            ### adalah hasil kalkulasi
             hasil[y, x] = pixel
 
+    ## Kembalikan citra hasil
     return hasil
 
-def threshold(citra: MatLike, T: int):
+# Fungsi invert thresholding dengan T = titik ambang
+def invert_threshold(citra: MatLike, T: int) -> MatLike:
+
+    ## Dapatkan tinggi dan lebar citra lalu buat citra hasil kosong
     h, w = citra.shape[:2]
     hasil = np.zeros((h, w), np.uint8)
 
+    ## Perulangan untuk setiap piksel pada citra
     for y in range(h):
         for x in range(w):
+
+            ### Jika nilai piksel lebih dari T, maka 0
+            ### Jika nilai piksel kurang dari sama dengan T, maka 1
             hasil[y, x] = 0 if citra[y, x] > T else 255
 
+    ## Kembalikan citra hasil
     return hasil
 
-def padding(citra: MatLike, thickness: int):
+# Fungsi untuk membuat bingkai pada citra dengan ketebalan tertentu
+def padding(citra: MatLike, thickness: int) -> MatLike:
+
+    ## Dapatkan tinggi dan lebar citra lalu buat citra hasil kosong
     h, w = citra.shape[:2]
+
+    ## Citra hasil memiliki tinggi dan lebar yang ditambahkan dengan
+    ## ketebalan yang diinginkan
     hasil = np.zeros((h + thickness * 2, w + thickness * 2), np.uint8)
 
+    ## Perulangan untuk setiap piksel pada citra
     for y in range(h):
         for x in range(w):
+
+            ### Piksel citra hasil memakai offset agar objek
+            ### berada di tengah
             hasil[y + thickness, x + thickness] = citra[y, x]
 
+    ## Kembalikan citra hasil
     return hasil
 
-def crop(citra: MatLike, thickness: int):
+# Fungsi untuk memotong bingkai pada citra dengan ketebalan tertentu
+def crop(citra: MatLike, thickness: int) -> MatLike:
+
+    ## Dapatkan tinggi dan lebar citra
     h1, w1 = citra.shape[:2]
+
+    ## Dapatkan tinggi dan lebar citra tanpa bingkai
     h2, w2 = h1 - thickness * 2, w1 - thickness * 2
+
+    ## Buat citra hasil kosong dengan tinggi dan lebar setelah crop
     hasil = np.zeros((h2, w2), np.uint8)
 
+    ## Perulangan untuk mendapatkan setiap piksel pada citra
     for y in range(h2):
         for x in range(w2):
+
+            ### Piksel citra hasil memakai offset karena
+            ### objek berada di tengah
             hasil[y, x] = citra[y + thickness, x + thickness]
 
+    ## Kembalikan citra hasil
     return hasil
 
-def bounding_box(citra: MatLike, contours: Sequence[MatLike]):
+# Fungsi bounding box untuk mendapatkan titik terluar dari kontur
+def bounding_box(citra: MatLike,
+                 contours: Sequence[MatLike]
+                 ) -> Tuple[int, int, int, int]:
 
-    # atas & kanan adalah nilai minimum jadi mulai dari ukuran citra
-    # bawah & kiri adalah nilai maksimum, jadi mulai dari 0
+    ## Atas & kiri adalah titik minimum, jadi mulai dari ukuran citra
     atas, kiri = citra.shape[:2]
+
+    ## Bawah & kanan adalah titik maksimum, jadi mulai dari 0
     bawah, kanan = 0, 0
 
-    # looping nyari titik terjauh atas bawah kanan kiri
+    ## Perulangan untuk setiap garis dari kontur
     for i in range(len(contours)):
         garis = contours[i]
 
+        ### Perulangan untuk setiap titik dari garis kontur
         for j in range(len(garis)):
+
+            #### Dapatkan titik x, y dari setiap titik garis
             x, y = garis[j][0]
 
+            #### Percabangan untuk menentukan titik terjauh
             if bawah < y: bawah = y
             if kanan < x: kanan = x
             if atas  > y: atas = y
             if kiri  > x: kiri = x
 
+    ## Kembalikan titik terjauh atas, kiri, bawah, dan kanan
     return atas, kiri, bawah, kanan
 
+# Fungsi menggambar kontur pada citra, seperti fungsi pada OpenCV
 def draw_contours(citra: MatLike, contours: Sequence[MatLike],
-                  color: Tuple[int, int, int], thickness: int):
+                  color: Tuple[int, int, int], thickness: int
+                  ) -> MatLike:
+
+    ## Buat citra hasil yang merupakan duplikat dari citra masukan
     hasil = citra.copy()
+
+    ## Gambar kontur pada citra hasil
     cv2.drawContours(hasil, contours, -1, color, thickness)
+
+    ## Kembalikan citra hasil
     return hasil
 
+# Fungsi menggambar kotak pada citra dengan 4 titik
 def draw_box(citra: MatLike, box: Tuple[int, int, int, int],
-             color: Tuple[int, int, int], thickness: int):
+             color: Tuple[int, int, int], thickness: int) -> MatLike:
+
+    ## Mendapatkan titik atas, kiri, bawah, dan kanan dari box
     atas, kiri, bawah, kanan = box
+
+    ## Buat citra hasil yang merupakan duplikat dari citra masukan
     hasil = citra.copy()
 
+    ## Menggambar 4 garis untuk membuat kotak
     cv2.line(hasil, (kiri, atas), (kanan, atas), color, thickness)
     cv2.line(hasil, (kanan, atas), (kanan, bawah), color, thickness)
     cv2.line(hasil, (kanan, bawah), (kiri, bawah), color, thickness)
     cv2.line(hasil, (kiri, bawah), (kiri, atas), color, thickness)
 
+    ## Kembalikan citra hasil
     return hasil
 
-def hitung_piksel(citra: MatLike, box: Tuple[int, int, int, int]):
+# Fungsi menghitung piksel pada citra dengan bounding box
+def hitung_piksel(citra: MatLike,
+                  box: Tuple[int, int, int, int]
+                  ) -> Tuple[int, int, int, float, float, float]:
+
+    ## Dapatkan setiap titik dari kotak
     atas, kiri, bawah, kanan = box
+
+    ## Definisikan variabel piksel, latar, dan objek
     px = 0
     bg = 0
     ob = 0
 
+    ## Perulangan untuk setiap piksel citra di dalam bounding box
     for y in range(atas, bawah):
         for x in range(kiri, kanan):
+
+            ### Menambahkan jumlah piksel
             px += 1
+
+            ### Jika piksel putih, maka termasuk objek
+            ### Jika piksel hitam, maka termasuk latar
             if citra[y, x]:
                 ob += 1
             else:
                 bg += 1
 
-    ## TAMPILKAN HASIL RASIO
+    ## Bulatkan perbandingan jika bilangan penyebut lebih dari 0
     bg_px = round(bg / px, 5) if px > 0 else 0
     ob_px = round(ob / px, 5) if px > 0 else 0
     bg_ob = round(bg / ob, 5) if ob > 0 else 0
 
+    ## Kembalikan jumlah piksel, latar, objek, dan perbandingannya
     return px, bg, ob, bg_px, ob_px, bg_ob
 
-def tampilkan(title: str, citra: MatLike, col=0):
-    cv2.namedWindow(title, cv2.WINDOW_NORMAL)
-    cv2.moveWindow(title, col % 3 * 500 + 50, col // 3 * 450 + 50)
-    cv2.resizeWindow(title, 400, 400)
-    cv2.imshow(title, citra)
+# Fungsi menampilkan citra pada window yang berderet
+def tampilkan(windows: Iterable[Tuple[str, MatLike]]):
+    i = 0
 
+    for title, citra in windows:
+        cv2.namedWindow(title, cv2.WINDOW_NORMAL)
+        cv2.moveWindow(title, i % 3 * 500 + 50, i // 3 * 450 + 50)
+        cv2.resizeWindow(title, 400, 400)
+        cv2.imshow(title, citra)
+        i += 1
+
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+# Fungsi utama klasifikasi yang menjalankan semua proses pada citra
 def klasifikasi(file_path: str, show=True, verbose=False):
+
+    ## Variabel apakah output pada terminal mendetail
     global __verbose
     __verbose = verbose
+
+    ## Mulai menghitung waktu mulai pengolahan
     start_time = time.process_time()
 
     ## Load
@@ -149,7 +239,7 @@ def klasifikasi(file_path: str, show=True, verbose=False):
 
     ### Thresholding invert
     log('Thresholding...')
-    hasil_threshold = threshold(hasil_grayscale, 225)
+    hasil_threshold = invert_threshold(hasil_grayscale, 225)
 
     ### Perbesar citra sebelum morfologi
     log('Perbesar citra...')
@@ -159,7 +249,10 @@ def klasifikasi(file_path: str, show=True, verbose=False):
     log('Morfologi closing...')
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (32, 32))
     hasil_closing = cv2.morphologyEx(
-        hasil_perbesar, cv2.MORPH_CLOSE, kernel, iterations=4
+        hasil_perbesar,
+        cv2.MORPH_CLOSE,
+        kernel,
+        iterations=4
     )
 
     ### Crop citra sesudah morfologi
@@ -171,15 +264,20 @@ def klasifikasi(file_path: str, show=True, verbose=False):
 
     ### KONTUR
     log('Mencari kontur...')
-    contours, _ = cv2.findContours(hasil_crop, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    hasil_kontur = draw_contours(hasil_crop, contours, (255, 255, 255), 2)
-    ekstraksi_fitur = draw_contours(ekstraksi_fitur, contours, (255, 255, 255), 2)
+    HITAM = (255, 255, 255)
+    contours, _ = cv2.findContours(
+        hasil_crop,
+        cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_NONE
+    )
+    hasil_kontur = draw_contours(hasil_crop, contours, HITAM, 2)
+    ekstraksi_fitur = draw_contours(ekstraksi_fitur, contours, HITAM, 2)
 
     ### MEMBUAT BOUNDING BOX DARI KONTUR
     log('Bounding box...')
     box = bounding_box(hasil_kontur, contours)
-    hasil_bounding = draw_box(hasil_kontur, box, (255, 255, 255), 2)
-    ekstraksi_fitur = draw_box(ekstraksi_fitur, box, (255, 255, 255), 2)
+    hasil_bounding = draw_box(hasil_kontur, box, HITAM, 2)
+    ekstraksi_fitur = draw_box(ekstraksi_fitur, box, HITAM, 2)
 
     ## Tampilkan waktu dibutuhkan untuk pengolahan
     elapsed = round(time.process_time() - start_time, 3)
@@ -197,21 +295,22 @@ def klasifikasi(file_path: str, show=True, verbose=False):
     print("Latar / Objek:", bg_ob)
     print(end='', flush=True)
 
-    ## TAMPILKAN CITRA
+    ## Tampilkan citra jika argumen show = True
     if show:
-        tampilkan('Citra Awal', citra, 0)
-        tampilkan('Grayscale', hasil_grayscale, 1)
-        tampilkan('Threshold', hasil_threshold, 2)
-        tampilkan('Perbesar', hasil_perbesar, 3)
-        tampilkan('Closing', hasil_closing, 4)
-        tampilkan('Ekstraksi Fitur', ekstraksi_fitur, 5)
-        cv2.waitKey()
-        cv2.destroyAllWindows()
+        tampilkan((
+            ('Citra Awal', citra),
+            ('Grayscale', hasil_grayscale),
+            ('Threshold', hasil_threshold),
+            ('Perbesar', hasil_perbesar),
+            ('Closing', hasil_closing),
+            ('Ekstraksi Fitur', ekstraksi_fitur),
+        ))
 
-    ## Return rasio background / piksel
+    ## Kembalikan rasio background / piksel
     return bg_px
 
+# Cek jika program dijalankan menggunakan perintah `python main.py`
 if __name__ == "__main__":
-    file_path = input('Lokasi citra untuk diklasifikasi (dataset/dataset-1.jpg): ') \
-    or 'dataset/dataset-1.jpg'
+    file_path = input('Lokasi citra (dataset/dataset-1.jpg): ') \
+        or 'dataset/dataset-1.jpg'
     klasifikasi(file_path, show=True, verbose=True)
